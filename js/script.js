b2bcb84bfe1c0679b6f8274fe2910220837c51eb
alex <gomelbox@gmail.com>
2019-06-17 20:03:12 +0300

    bash test

---
+++ js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+
+
+var leftBorderWidth = 1;
+let second = 2;
+
+const pi = 3.14;
+
+let persone = {
+    name: "Arkadiy",
+    age: 100,
+    isMarried: false
+};
+
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.gif']
+
+console.log(arr[0]);
+
+// alert ('Hello wall');
+
+//let answer = confirm ("Are you girl?");
+
+//console.log(answer);
+
+//let answer = +prompt("Сколько вам лет?", "Yes");
+
+//console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isCheked = true,
+    isClosed = false;
+
+console.log(isCheked || isClosed);
